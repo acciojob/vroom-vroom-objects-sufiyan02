5cd0968397b5b0ddf9e0f03b84ad47c6c6f27478
Sufiyan Munshi <101869552+sufiyan02@users.noreply.github.com>
2025-02-11 10:05:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 // Complete the js code
-function Car(make, model) {}
+// function Car(make, model) {}
 
-function SportsCar(make, model, topSpeed) {}
+// function SportsCar(make, model, topSpeed) {}
+
+class Car {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
+
+  // Method to return make and model
+  getMakeModel() {
+    return `${this.make} ${this.model}`;
+  }
+}
+
+
+class SportsCar extends Car {
+  constructor(make, model, topSpeed) {
+    super(make, model);
+    this.topSpeed = topSpeed;
+  }
+
+
+  getTopSpeed() {
+    return this.topSpeed;
+  }
+}
 
 // Do not change the code below
 window.Car = Car;
